d59b4eede24d69ac0fa7040088b6dbd51a212d23
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-02-15 05:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+var todo=document.getElementById("newTodoInput");
+var btn=document.getElementById("addTodoBtn");
+var ol=document.getElementById("todoList");
+
+function addTodo(){
+	 
+	if(todo.value){
+		var li=document.createElement("li");
+		li.textContent=todo.value;
+		ol.appendChild(li);
+		todo.value="";
+	}
+}
+
+btn.addEventListener('click',addTodo);
